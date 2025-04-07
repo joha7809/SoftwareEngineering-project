fa0a91601ff0ddb262cbf293033008210b406536
joha7809 <joha7809@gmail.com>
2025-04-07 16:54:51 +0200

    WIP

--- src/test/java/hellocucumber/ChangeStartDateOfActivitySteps.java
+++ src/test/java/hellocucumber/ChangeStartDateOfActivitySteps.java
@@ -1,65 +1,65 @@
-package hellocucumber;
+// package hellocucumber;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+// import io.cucumber.java.en.Given;
+// import io.cucumber.java.en.Then;
+// import io.cucumber.java.en.When;
 
-public class ChangeStartDateOfActivitySteps {
+// public class ChangeStartDateOfActivitySteps {
 
-    private boolean userLoggedIn;
-    private boolean activityExists;
-    private String startDate;
-    private String errorMessage;
+//     private boolean userLoggedIn;
+//     private boolean activityExists;
+//     private String startDate;
+//     private String errorMessage;
 
-    @Given("user is logged in")
-    public void userIsLoggedIn() {
-        this.userLoggedIn = true;
-    }
+//     @Given("user is logged in")
+//     public void userIsLoggedIn() {
+//         this.userLoggedIn = true;
+//     }
 
-    @Given("an activity exists")
-    public void anActivityExists() {
-        this.activityExists = true;
-    }
+//     @Given("an activity exists")
+//     public void anActivityExists() {
+//         this.activityExists = true;
+//     }
 
-    @Given("start date is set")
-    public void startDateIsSet() {
-        this.startDate = "2025-04-01"; // Example initial start date
-    }
+//     @Given("start date is set")
+//     public void startDateIsSet() {
+//         this.startDate = "2025-04-01"; // Example initial start date
+//     }
 
-    @Given("start date is not set")
-    public void startDateIsNotSet() {
-        this.startDate = null;
-    }
+//     @Given("start date is not set")
+//     public void startDateIsNotSet() {
+//         this.startDate = null;
+//     }
 
-    @When("user updates the start date")
-    public void userUpdatesTheStartDate() {
-        if (userLoggedIn && activityExists && startDate != null) {
-            startDate = "2025-04-10"; // Example updated start date
-        } else {
-            errorMessage = "Error: Cannot update start date";
-        }
-    }
+//     @When("user updates the start date")
+//     public void userUpdatesTheStartDate() {
+//         if (userLoggedIn && activityExists && startDate != null) {
+//             startDate = "2025-04-10"; // Example updated start date
+//         } else {
+//             errorMessage = "Error: Cannot update start date";
+//         }
+//     }
 
-    @When("the user updates the start date to {string}")
-    public void theUserUpdatesTheStartDateTo(String newStartDate) {
-        if (userLoggedIn && activityExists) {
-            if (newStartDate.isEmpty()) {
-                errorMessage = "Error: Start date cannot be empty";
-            } else {
-                startDate = newStartDate;
-            }
-        } else {
-            errorMessage = "Error: Cannot update start date";
-        }
-    }
+//     @When("the user updates the start date to {string}")
+//     public void theUserUpdatesTheStartDateTo(String newStartDate) {
+//         if (userLoggedIn && activityExists) {
+//             if (newStartDate.isEmpty()) {
+//                 errorMessage = "Error: Start date cannot be empty";
+//             } else {
+//                 startDate = newStartDate;
+//             }
+//         } else {
+//             errorMessage = "Error: Cannot update start date";
+//         }
+//     }
 
-    @Then("the start date is updated")
-    public void theStartDateIsUpdated() {
-        assert startDate.equals("2025-04-10"); // Ensure the start date was updated
-    }
+//     @Then("the start date is updated")
+//     public void theStartDateIsUpdated() {
+//         assert startDate.equals("2025-04-10"); // Ensure the start date was updated
+//     }
 
-    @Then("a error message is printed")
-    public void aErrorMessageIsPrinted() {
-        assert errorMessage != null && !errorMessage.isEmpty();
-    }
-}
+//     @Then("a error message is printed")
+//     public void aErrorMessageIsPrinted() {
+//         assert errorMessage != null && !errorMessage.isEmpty();
+//     }
+// }
--- src/test/java/hellocucumber/ProjectSteps.java
+++ src/test/java/hellocucumber/ProjectSteps.java
@@ -1,45 +1,68 @@
 package hellocucumber;
 
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import dtu.example.Controller.AppManager;
+import dtu.example.Model.Project;
+import dtu.example.Model.User;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class ProjectSteps {
+    public AppManager manager = new AppManager();
+    public String projectIDCreated;
+    public Project projectCreated;
     
     @Given("a user is logged in")
     public void a_user_is_logged_in() {
-        // Write code here that turns the phrase above into concrete actions
+        // create a user and set it as the active user
+        User user = new User("testUser");
+        manager.setActiveUser(user);
+        // check that the active user is not null        
+        assertTrue(manager.activeUser != null);
     }
 
     @Given("there is no Project with the name {string}")
-    public void there_is_no_Project_with_the_name(String s) {
+    public void there_is_no_Project_with_the_name(String ProjectName) {
         // Write code here that turns the phrase above into concrete actions
+
+        assertTrue(manager.getProject(ProjectName) == null); //Ensure that there is no project called ProjectName
+    }
+
+    @Given("there is a Project with the name {string}")
+    public void there_is_a_Project_with_the_name(String projectID) {
+        // Write code here that turns the phrase above into concrete actions
+        Project project = new Project(projectID);
+        // add the project to the manager
+        manager.addProject(project);
+        // check that the project is in the manager
+        assertTrue(manager.getProject(projectID) != null);
+    }
+
+
+
+    @When("a Project with name {string} is created")
+    public void a_Project_with_name_is_created(String s) {
+        projectIDCreated = s;
+        projectCreated = new Project(s);
     }
 
     @Then("the Project is added to the list of projects")
     public void the_Project_is_added_to_the_list_of_projects() {
-        // Write code here that turns the phrase above into concrete actions
+        // add the project to the manager
+        manager.addProject(projectCreated);
+        // check that the project is in the manager
+        assertTrue(manager.getProject(projectIDCreated).getProjectID().equals(projectIDCreated));
     }
 
-    @When("a Project with name {string} is created")
-    public void a_Project_with_name_is_created(String s) {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @When("a Project with the name {string} is created")
-    public void a_Project_with_the_name_is_created(String s) {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @Given("there is a Project with the name {string}")
-    public void there_is_a_Project_with_the_name(String s) {
-        // Write code here that turns the phrase above into concrete actions
-    }
 
     @Then("an error message is printed")
     public void an_error_message_is_printed() {
-        // Write code here that turns the phrase above into concrete actions
-    }
+        // When adding project to manager list, an error should occur.
+        manager.addProject(projectCreated); //Should fail
 
+
+    }
 }
