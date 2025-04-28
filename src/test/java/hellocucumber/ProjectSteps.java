549773b6f201fb3f324f3d3280b7f6777e593fa4
Ke-rem <142585744+Ke-rem@users.noreply.github.com>
2025-04-28 13:38:41 +0200

    Fix conflict

--- src/test/java/hellocucumber/ProjectSteps.java
+++ src/test/java/hellocucumber/ProjectSteps.java
@@ -1,57 +1,52 @@
 package hellocucumber;
 
 
-import io.cucumber.java.en.Given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//import dtu.example.model.Project;
+//import dtu.example.ui.ProjectOrganizer;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class ProjectSteps {
-    public AppManager manager = new AppManager();
-    public String projectIDCreated;
-    public Project projectCreated;
     
-    @Given("a user is logged in")
-    public void a_user_is_logged_in() {
+    //ProjectOrganizer projectOrganizer = new ProjectOrganizer();
+
+    @When("a Project is created with a name {string}")
+    public void aProjectIsCreatedWithAName(String string) 
+    {
+       // projectOrganizer.createProject(string);
+    }
+
+    @Then("the Projects name {string}")
+    public void theProjectsName(String string) 
+    {
+        //assertEquals(string, projectOrganizer.getProject().getProjectName());
+    }
+
+    @Given("a user {string} is logged in")
+    public void a_user_is_logged_in(String s) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Then("the Project is added to the list of projects")
+    public void the_Project_is_added_to_the_list_of_projects() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("a Project with name {string} is created")
+    public void a_Project_with_name_is_created(String s) {
         // Write code here that turns the phrase above into concrete actions
     }
 
     @Given("there is no Project with the name {string}")
     public void there_is_no_Project_with_the_name(String s) {
         // Write code here that turns the phrase above into concrete actions
-
-        assertTrue(manager.getProject(ProjectName) == null); //Ensure that there is no project called ProjectName
     }
 
-    @Given("there is a Project with the name {string}")
-    public void there_is_a_Project_with_the_name(String projectID) {
-        // Write code here that turns the phrase above into concrete actions
-        Project project = new Project(projectID);
-        // add the project to the manager
-        manager.addProject(project);
-        // check that the project is in the manager
-        assertTrue(manager.getProject(projectID) != null);
-    }
-
-
-
-    @When("a Project with name {string} is created")
-    public void a_Project_with_name_is_created(String s) {
-        projectIDCreated = s;
-        projectCreated = new Project(s);
-    }
-
-    @When("a Project with the name {string} is created")
-    public void a_Project_with_the_name_is_created(String s) {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @Given("there is a Project with the name {string}")
-    public void there_is_a_Project_with_the_name(String s) {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @Then("an error message is printed")
-    public void an_error_message_is_printed() {
+    @Given("a user is logged in")
+    public void a_user_is_logged_in() {
         // Write code here that turns the phrase above into concrete actions
     }
 }
