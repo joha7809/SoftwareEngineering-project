db601896b2bf984934c875061905098609bc3a9c
AugustWFOE <augustfeentved@gmail.com>
2025-05-05 15:08:04 +0200

    createproject ui command

--- src/main/java/dtu/example/Controller/commands/CommandRegistry.java
+++ src/main/java/dtu/example/Controller/commands/CommandRegistry.java
@@ -9,8 +9,10 @@ public class CommandRegistry {
     public CommandRegistry(AppManager controller){
         // Init all commands
         Login login = new Login(controller);
+        CreateProject createProject = new CreateProject(controller);
 
         this.registerCommand(login);
+        this.registerCommand(createProject);
     }
 
     public void registerCommand(CommandInterface<?> command){
