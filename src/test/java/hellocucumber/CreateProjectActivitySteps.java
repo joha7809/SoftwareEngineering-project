e2e5001ee77caec0e4143667a6d8d70b1ff0883c
BobVonAnd <s245909@dtu.dk>
2025-05-06 15:42:33 +0200

    Co-authored-by: Johannes Sigvardsen <joha7809@users.noreply.github.com>

--- src/test/java/hellocucumber/CreateProjectActivitySteps.java
+++ src/test/java/hellocucumber/CreateProjectActivitySteps.java
@@ -1,14 +1,26 @@
 package hellocucumber;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import dtu.example.Controller.AppManager;
+import dtu.example.model.Project;
+import dtu.example.model.User;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.lu.a;
 
 public class CreateProjectActivitySteps {
-    private final AppManager appManager = new AppManager();
+
+    private final SharedContext sharedContext;
+    private final AppManager appManager;
+
+    public CreateProjectActivitySteps(SharedContext sharedContext) {
+        this.sharedContext = sharedContext;
+        this.appManager = sharedContext.getAppManager();
+    }
 
     @Given("a project called {string} exists")
     public void a_project_called_exists(String s) {
@@ -20,11 +32,20 @@ public class CreateProjectActivitySteps {
     @Given("user {string} is not project leader for {string}")
     public void user_is_not_project_leader_for(String s, String s2) {
         // Write code here that turns the phrase above into concrete actions
+        Project project = appManager.getProjectById(s2);
+        User user = appManager.getUser(s);
+        assertTrue(project != null);
+        assertTrue(user != null);
+        
+        assertFalse(project.getProjectLead().equals(user));
     }
 
     @Given("the user {string} is project leader for {string}")
     public void the_user_is_project_leader_for(String s, String s2) {
         // Write code here that turns the phrase above into concrete actions
+        appManager.createUser(s);
+        appManager.getProjectById(s2).setProjectLead(appManager.getUser(s));
+        assertTrue(appManager.getProjectById(s2).getProjectLead().equals(s));
     }
 
    
@@ -32,6 +53,9 @@ public class CreateProjectActivitySteps {
     @Given("the project has a project leader for {string}")
     public void the_project_has_a_project_leader_for(String s) {
         // Write code here that turns the phrase above into concrete actions
+        appManager.createUser("ole");
+        appManager.getProjectById(s).setProjectLead(appManager.getUser("ole"));
+        assertTrue(!appManager.getProjectById(s).getProjectLead().equals(null));
     }
 
     @Given("project {string} has no project lead")
