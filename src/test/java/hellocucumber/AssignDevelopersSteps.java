cb9221419d46408dc3195e112c9eefc4c1b2fc7b
joha7809 <joha7809@gmail.com>
2025-05-08 14:41:32 +0200

    Fix all

--- src/test/java/hellocucumber/AssignDevelopersSteps.java
+++ src/test/java/hellocucumber/AssignDevelopersSteps.java
@@ -1,50 +1,48 @@
-// package hellocucumber;
+package hellocucumber;
 
-// import static org.junit.jupiter.api.Assertions.assertFalse;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// import dtu.example.Controller.AppController;
-// import dtu.example.Controller.command_returns.StatusMessage;
-// import dtu.example.model.Project;
-// import dtu.example.model.ProjectActivity;
-// import dtu.example.model.User;
-// import dtu.example.ui.App;
-// import io.cucumber.java.en.Given;
-// import io.cucumber.java.en.Then;
-// import io.cucumber.java.en.When;
-// import io.cucumber.java.lu.a;
+import dtu.example.Controller.AppController;
+import dtu.example.Controller.command_returns.StatusMessage;
+import dtu.example.model.Project;
+import dtu.example.model.ProjectActivity;
+import dtu.example.model.User;
+import dtu.example.ui.App;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.lu.a;
 
-// public class AssignDevelopersSteps {
+public class AssignDevelopersSteps {
 
-//     private final SharedContext sharedContext;
-//     private final AppController controller;
-//     private StatusMessage result;
+    private final SharedContext sharedContext;
+    private final AppController controller;
+    private StatusMessage result;
 
-//     public AssignDevelopersSteps(SharedContext sharedContext)
-//     {
-//         this.sharedContext = sharedContext;
-//         this.controller = sharedContext.getController();
+    public AssignDevelopersSteps(SharedContext sharedContext) {
+        this.sharedContext = sharedContext;
+        this.controller = sharedContext.getController();
+    }
 
-//     }
+    private String activityName = "Metting";
+    private Project project;
+    private User user;
+    private ProjectActivity activity;
 
-//     private String activityName = "Metting";
-//     private Project project;
-//     private User user;
-//     private ProjectActivity activity;
+    @Given("user {string} is project lead on project {string}")
+    public void isTheProjectLeader(String userInitials, String projectName) {
+        controller.createUser(userInitials);
+        controller.createProject(projectName);
+        controller.createProjectActivity(projectName, activityName);
+        controller.setActiveUser(controller.getUser(userInitials));
 
-//     @Given("user {string} is project lead on project {string}")
-//     public void isTheProjectLeader(String userInitials, String projectName) {
-//         controller.createUser(userInitials);
-//         controller.createProject(projectName);
-//         controller.createProjectActivity(projectName, activityName);
-//         controller.setActiveUser(controller.getUser(userInitials));
+        user = controller.getUser(userInitials);
+        project = controller.getProject(projectName);
+        activity = controller.getProjectActivity(projectName, activityName);
 
-//         user = controller.getUser(userInitials);
-//         project = controller.getProject(projectName);
-//         activity = controller.getProjectActivity(projectName, activityName);
-
-//         assertTrue(controller.getActiveUser().equals(userInitials));
-//     }
+        assertTrue(controller.getActiveUser().equals(userInitials));
+    }
 
     @Given("user {string} is unavailable")
     public void isUnavailable(String userInitials) {
@@ -69,7 +67,7 @@
         }
         assertFalse(isAvailable);
 
-//     }
+    }
 
     @Given("user {string} is available")
     public void is_available(String userInitials) {
@@ -101,21 +99,22 @@
         assertTrue(isAssign);
     }
 
-//     @Then("{string} is assigned to the activity")
-//     public void is_assigned_to_the_activity(String userInitals) {
-//         // Write code here that turns the phrase above into concrete actions
-//         for (ProjectActivity activity : controller.getUser(user.getUserID()).getJoinedActivities()) {
-//             assertTrue(activity.getName().equals(activityName));
-//         }
-//     }
+    @Then("{string} is assigned to the activity")
+    public void is_assigned_to_the_activity(String userInitals) {
+        // Write code here that turns the phrase above into concrete actions
+        for (ProjectActivity activity : controller.getUser(user.getUserID()).getJoinedActivities()) {
+            assertTrue(activity.getName().equals(activityName));
+        }
+    }
 
 
 
-//     @Then("{string} is not assigned to the activity")
-//     public void is_not_assigned_to_the_activity(String userInitials) {
-//         // Write code here that turns the phrase above into concrete actions
-//         for (ProjectActivity activity : controller.getUser(user.getUserID()).getJoinedActivities()) {
-//             assertFalse(activity.getName().equals(activityName));
-//         }
-//     }
-// }
+    @Then("{string} is not assigned to the activity")
+    public void is_not_assigned_to_the_activity(String userInitials) {
+        // Write code here that turns the phrase above into concrete actions
+        for (ProjectActivity activity : controller.getUser(user.getUserID()).getJoinedActivities()) {
+            assertFalse(activity.getName().equals(activityName));
+        }
+    }
+}
+
--- src/test/java/hellocucumber/CreateFixedActivitySteps.java
+++ src/test/java/hellocucumber/CreateFixedActivitySteps.java
@@ -1,46 +1,37 @@
-// package hellocucumber;
+package hellocucumber;
 
-// import io.cucumber.java.en.Given;
-// import io.cucumber.java.en.Then;
-// import io.cucumber.java.en.When;
-// import dtu.example.Controller.AppController;
-// import dtu.example.Controller.command_returns.StatusMessage;
-// import dtu.example.model.User;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import dtu.example.Controller.AppController;
+import dtu.example.Controller.command_returns.StatusMessage;
+import dtu.example.model.User;
 
 
-// import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-// public class CreateFixedActivitySteps {
-//     private final SharedContext sharedContext;
-//     private final AppController controller;
-//     private StatusMessage result;
+public class CreateFixedActivitySteps {
+    private final SharedContext sharedContext;
+    private final AppController controller;
+    private StatusMessage result;
 
-//     public CreateFixedActivitySteps(SharedContext sharedContext)
-//     {
-//         this.sharedContext = sharedContext;
-//         this.controller = sharedContext.getController();
+    public CreateFixedActivitySteps(SharedContext sharedContext)
+    {
+        this.sharedContext = sharedContext;
+        this.controller = sharedContext.getController();
 
-//     }
-    
-
-    // @When("the user types the command {string} and arguments timeStart {string} timeEnd {string} type {string}")
-    // public void the_user_types_the_command_and_arguments_timeStart_timeEnd_type(String command, String timeStart, String timeEnd, String type) {
-    //     // Write code here that turns the phrase above into concrete actions
-    //     result = controller.createFixedActivity(command, timeStart, timeEnd, type);
-    //     sharedContext.setResult(result);
-    // }
+    }
 
     @When("the user creates a fixed activity with timeStart {string} timeEnd {string} type {string}")
     public void the_user_types_the_command_and_arguments_timeStart_timeEnd_type(String timeStart, String timeEnd, String type) {
         // Write code here that turns the phrase above into concrete actions
-        //result = controller.createFixedActivity(command, timeStart, timeEnd, type);
         result = controller.createFixedActivity(timeStart, timeEnd, type);
         sharedContext.setResult(result);
     }
 
-//     @Then("a fixed activity is created with the start date {string}, end date {string} and type {string}")
-//     public void a_fixed_activity_is_created_with_the_user_start_date_end_date_and_type(String timeStart, String timeEnd, String type) {
-//         // TODO: Implement logic to verify the fixed activity creation
+    @Then("a fixed activity is created with the start date {string}, end date {string} and type {string}")
+    public void a_fixed_activity_is_created_with_the_user_start_date_end_date_and_type(String timeStart, String timeEnd, String type) {
+        // TODO: Implement logic to verify the fixed activity creation
         
         
         for (int i = 0; i < controller.getFixedActivities().size(); i++){
@@ -53,9 +44,4 @@
         assertTrue(sharedContext.getResult().success);
     }
 
-    //@Then("the system displays an error message: {string}")
-    //public void the_system_displays_an_error_message(String expectedMessage) {
-        // TODO: Implement logic to verify the error message
-    //}
-
-// }
+}
